b912cec66b01873ecc72a78a68de7bf8e6833423
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2024-12-31 11:02:07 -0400

    Se creo el  Componente Data Section 4.
    Este contiene los datos que se muestran  en la section 4.

---
+++ src/components/data/Section-main4.js
@@ -0,0 +1,43 @@
+const SectionDm4 = [
+    {
+        class: "Web",
+        pageName: "Kfc Dominican Republic",
+        summary: "Project was about precision and information....",
+        image: "src/assets/KFC.png",
+        imgName: "Kfc img",
+        link: "https://urpirio.github.io/KFC-RD/",
+    },
+    {
+        class: "Web2",
+        pageName: "Simply Company",
+        summary: "Project was about precision and information....",
+        image: "src/assets/SimplyCompany.png",
+        imgName: "Simply Company img",
+        link: "https://urpirio.github.io/Simply-Company/",
+    },
+    {
+        class: "Web3",
+        pageName: "G4 Devs",
+        summary: "Project was about precision and information....",
+        image: "src/assets/G4Devs.png",
+        imgName: "G4 Devs img",
+        link: "https://urpirio.github.io/UrpDev/",
+    },
+    {
+        class: "Web4",
+        pageName: "UrpSoft",
+        summary: "Project was about precision and information....",
+        image: "src/assets/UrpSoft.png",
+        imgName: "UrpSoft I=img",
+        link: "https://urpirio.github.io/UrpSoft-2.0/",
+    },
+    {
+        class: "NoContent",
+        pageName: "Git hud",
+        summary: "Visit git hub for more content.",
+        image: "https://logos-world.net/wp-content/uploads/2020/11/GitHub-Logo.png",
+        imgName: "Git hub logo",
+        link: "https://github.com/Urpirio?tab=repositories",
+    }
+]
+export default SectionDm4
