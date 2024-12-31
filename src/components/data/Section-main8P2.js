163bd36523ad0e8a9b9671ddfbbbddecf847c3fe
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2024-12-31 11:11:33 -0400

    Se creo el Componente de la Section 8P2
    Aqui se encuentran todos los datos de la Section 8P2.

---
+++ src/components/data/Section-main8P2.js
@@ -0,0 +1,22 @@
+const SectionDm8P2 = [
+    {
+        TypeIcon: "bx bx-phone-call",
+        TypeContact: "Phone",
+        Typelink: "tel:01 123 654 8096",
+        InfoContact: "+01 123 654 8096",
+    },
+    {
+        TypeIcon: "bx bx-envelope",
+        TypeContact: "Email",
+        Typelink: "mailto:Urpiriojunior@gmail.com",
+        InfoContact: "Urpiriojunior@gmail.com",
+    },
+    {
+        TypeIcon: "bx bxs-map",
+        TypeContact: "Address",
+        Typelink: "https://maps.app.goo.gl/C1XmXaLcPahDv58A6",
+        InfoContact: "Santo Domingo, Distrito Nacional ",
+    },
+
+];
+export default SectionDm8P2;
