4976b2dac6eaa3e7660f5f14aaf1262034891c0b
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2024-12-31 11:26:10 -0400

    Se creo el Componente Section 3
    Este Contienen los servicios que ofrece la compañía.

---
+++ src/components/main/Section-main3.jsx
@@ -0,0 +1,11 @@
+
+
+const Section_main3 = () =>{
+    return(
+         <div className="Section-main3-div1">
+            <h1>My Quality Services</h1>
+            <p>We put your ideas and thus your wishes in the form of a unique web project that inspires you and you customers.</p>
+         </div>
+    )
+}
+export default Section_main3
