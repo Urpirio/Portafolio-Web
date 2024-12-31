0b15c52791e582a531dbb00ed3fa938438a21686
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2024-12-31 11:07:13 -0400

    Se creo el Componene Data Section 6
    Contiene todos los datos de las Habilidades mostradas en la section 6.

---
+++ src/components/data/Section-main6.js
@@ -0,0 +1,33 @@
+const SectionDm6 = [
+    {
+        logo: "src/assets/figma.png",
+        deplogo: "figma logo",
+        percent: "80%",
+        tecnology: "Figma",
+    },
+    {
+        logo: "src/assets/nodejs.png",
+        deplogo: "nodejs logo",
+        percent: "90%",
+        tecnology: "Nodejs",
+    },
+    {
+        logo: "src/assets/react.png",
+        deplogo: "React logo",
+        percent: "95%",
+        tecnology: "React",
+    },
+    {
+        logo: "src/assets/js.png",
+        deplogo: "Javascripts logo",
+        percent: "95%",
+        tecnology: "Javascripts",
+    },
+    {
+        logo: "src/assets/python.webp",
+        deplogo: "Python logo",
+        percent: "80%",
+        tecnology: "Python",
+    }
+]
+export default SectionDm6
