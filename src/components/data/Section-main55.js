f4e6c28e900586f73db3c3272f51d7ddb35fc8ee
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2024-12-31 11:05:38 -0400

    Se Creo el Componentes de Datos Section 5 P2.
    Este contiene todos los datos del apartado numero dos de la section 5, en especifico los datos de educación.

---
+++ src/components/data/Section-main55.js
@@ -0,0 +1,18 @@
+const SectionDm55 = [
+    {
+        time: "September 2022 - Present",
+        title: "Software ingeniere",
+        place: "University Dominico Americano",
+    },
+    {
+        time: "Jenuary 2023 - november 2023",
+        title: "English immersion program",
+        place: "Dominico Americano",
+    },
+    {
+        time: "January 2024 - April 2024",
+        title: "Diploma in Science,Technology and Engineering",
+        place: "Galway, Irlanda",
+    },
+]
+export default SectionDm55
