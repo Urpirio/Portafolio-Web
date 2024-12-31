3d066d8e4d1d9662949084ec3123e50469c82c50
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2024-12-31 11:08:01 -0400

    Se creo Componente Data Section 7
    Este contiene todos los datos de los Clientes mostrados en dicha section.

---
+++ src/components/data/Section-main7.js
@@ -0,0 +1,27 @@
+const SectionDm7 = [
+    {
+        clientePhoto: "src/assets/PersonServices/adubajala-services.jpg",
+        clientName: "Abubajala Dudulu",
+        positionWork: "Director ejecutivo (CEO)",
+        opinion: "UrpirioDev is a professional Designer he really helps my business by providing value to my business.",
+    },
+    {
+        clientePhoto: "src/assets/PersonServices/Jeffry-services.jpg",
+        clientName: "Jeffry Thomson",
+        positionWork: "Jefe del departamento de Tecnologia",
+        opinion: "UrpirioDev is a professional Designer he really helps my business by providing value to my business.",
+    },
+    {
+        clientePhoto: "src/assets/PersonServices/Maria-services.jpg",
+        clientName: "Maria Rodriguez",
+        positionWork: "Gerente del banco BHD",
+        opinion: "UrpirioDev is a professional Designer he really helps my business by providing value to my business.",
+    },
+    {
+        clientePhoto: "src/assets/PersonServices/Pedro-services.jpg",
+        clientName: "Pedro Corripio",
+        positionWork: "Vicepresidente De Intrustricas corripio",
+        opinion: "UrpirioDev is a professional Designer he really helps my business by providing value to my business.",
+    }
+];
+export default SectionDm7;
