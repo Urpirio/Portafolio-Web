0a5ae7f99a38254c54fa164b591e52967a3a1ed6
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2024-12-31 11:00:47 -0400

    Se creo el Componente Data Section 3
    Este contiene la información que se muestra en la section 3.

---
+++ src/components/data/Section-main3.jsx
@@ -0,0 +1,35 @@
+const SectionDm3 = [
+    {
+        function: "Container_sm3_F1",
+        id: "Container-sm3-1",
+        Number: "01",
+        title:"Web Development",
+        content:"Conducting qualitative and quantitative research to understand user needs, behaviors, and pain points. Utiling methods..",
+        link:"",
+    },
+    {
+        function: "{Container_sm3_F2}",
+        id: "Container-sm3-2",
+        Number: "02",
+        title:"UI/UX Design",
+        content:"Conducting qualitative and quantitative research to understand user needs, behaviors, and pain points. Utiling methods..",
+        link:"",
+    },
+    {
+        function: "{Container_sm3_F3}",
+        id: "Container-sm3-3",
+        Number: "03",
+        title:"Content Writing",
+        content:"Conducting qualitative and quantitative research to understand user needs, behaviors, and pain points. Utiling methods..",
+        link:"",
+    },
+    {
+        function: "{Container_sm3_F4}",
+        id: "Container-sm3-4",
+        Number: "04",
+        title:"Digital Marketing",
+        content:"Conducting qualitative and quantitative research to understand user needs, behaviors, and pain points. Utiling methods..",
+        link:"",
+    }
+]
+export default SectionDm3
