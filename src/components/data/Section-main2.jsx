f7ebe3f371ca2f26425a25aa7198d13627230ff6
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2024-12-31 10:59:50 -0400

    Se creo el componente Datos Section 2..
    Este contiene los datos que se ven en la section 2.

---
+++ src/components/data/Section-main2.jsx
@@ -0,0 +1,19 @@
+
+
+const SectionDm2 = [
+    {
+        Numbers: "1+",
+        title: "Years of Experience",
+        
+    },
+    {
+        Numbers: "10+",
+        title: "Project Completed", 
+    },
+    {
+        Numbers: "50+",
+        title: "Happy Clients", 
+    }
+]
+
+export default SectionDm2
