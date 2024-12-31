a2fb073f93feddffc06851851a2bcbfed1e6d882
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2024-12-31 11:23:27 -0400

    Se Creo el Componente Section 2
    Este es un Complemento de la section 1, Este se encarga de mostrar La cantidad de clientes satisfechos, Años de experiencia y proyectos Concluidos.

---
+++ src/components/main/Section-main2.jsx
@@ -0,0 +1,17 @@
+
+
+
+
+const Section_main2 = (props) => {
+
+
+    return(
+
+             <div> 
+                <h2>{props.number}</h2>
+                <p>{props.title}</p>
+                </div>
+    
+    )
+}
+export default  Section_main2
