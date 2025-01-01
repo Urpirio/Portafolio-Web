74587c8bcc164948bf0ed73ce8674753356c52d8
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-01-01 04:44:04 -0400

    Se agrego nueva estructura al Componente para garantizar agregar animaciones de transicion.

--- src/components/main/Section-main4.jsx
+++ src/components/main/Section-main4.jsx
@@ -10,10 +10,10 @@ const Section_main4 = () =>{
         document.getElementById("Web4").style.display = "flex";
         document.getElementById("NoContent").style.display = "none";
 
-        let All = document.getElementById("All").style.background = "linear-gradient(90deg,blue,#0077b6,#90e0ef)";
-        let Apps = document.getElementById("Apps").style.background = "none";
-        let Webs = document.getElementById("Webs").style.background = "none";
-        let Expe = document.getElementById("UX_UI").style.background = "none";
+        let Apps = document.getElementById("Sm4-background-All").style.background = "linear-gradient(90deg,blue,#0077b6,#90e0ef)";
+        let All = document.getElementById("Sm4-background-Apps").style.background = "none";
+        let Webs = document.getElementById("Sm4-background-Webs").style.background = "none";
+        let Expe = document.getElementById("Sm4-background-UX_UI").style.background = "none";
     }
     function Apps(){
 
@@ -23,10 +23,10 @@ const Section_main4 = () =>{
         document.getElementById("Web4").style.display = "none";
         document.getElementById("NoContent").style.display = "flex";
 
-        let All = document.getElementById("All").style.background = "none";
-        let Apps = document.getElementById("Apps").style.background = "linear-gradient(90deg,blue,#0077b6,#90e0ef)";
-        let Webs = document.getElementById("Webs").style.background = "none";
-        let Expe = document.getElementById("UX_UI").style.background = "none";
+        let Apps = document.getElementById("Sm4-background-All").style.background = "none";
+        let All = document.getElementById("Sm4-background-Apps").style.background = "linear-gradient(90deg,blue,#0077b6,#90e0ef)";
+        let Webs = document.getElementById("Sm4-background-Webs").style.background = "none";
+        let Expe = document.getElementById("Sm4-background-UX_UI").style.background = "none";
     }
     function Webs(){
         document.getElementById("Web").style.display = "flex";
@@ -35,10 +35,10 @@ const Section_main4 = () =>{
         document.getElementById("Web4").style.display = "flex";
         document.getElementById("NoContent").style.display = "none";
 
-        let Apps = document.getElementById("All").style.background = "none";
-        let All = document.getElementById("Apps").style.background = "none";
-        let Webs = document.getElementById("Webs").style.background = "linear-gradient(90deg,blue,#0077b6,#90e0ef)";
-        let Expe = document.getElementById("UX_UI").style.background = "none"
+        let Apps = document.getElementById("Sm4-background-All").style.background = "none";
+        let All = document.getElementById("Sm4-background-Apps").style.background = "none";
+        let Webs = document.getElementById("Sm4-background-Webs").style.background = "linear-gradient(90deg,blue,#0077b6,#90e0ef)";
+        let Expe = document.getElementById("Sm4-background-UX_UI").style.background = "none";
     }
     function Expe(){
         document.getElementById("Web").style.display = "none";
@@ -47,10 +47,10 @@ const Section_main4 = () =>{
         document.getElementById("Web4").style.display = "none";
         document.getElementById("NoContent").style.display = "flex";
 
-        let Apps = document.getElementById("All").style.background = "none";
-        let All = document.getElementById("Apps").style.background = "none";
-        let Webs = document.getElementById("Webs").style.background = "none";
-        let Expe = document.getElementById("UX_UI").style.background = "linear-gradient(90deg,blue,#0077b6,#90e0ef)";
+        let Apps = document.getElementById("Sm4-background-All").style.background = "none";
+        let All = document.getElementById("Sm4-background-Apps").style.background = "none";
+        let Webs = document.getElementById("Sm4-background-Webs").style.background = "none";
+        let Expe = document.getElementById("Sm4-background-UX_UI").style.background = "linear-gradient(90deg,blue,#0077b6,#90e0ef)";
     }
 
 
@@ -59,10 +59,10 @@ const Section_main4 = () =>{
         <div className="Section-main4-div1">
             <h1>My Recent Works</h1>
             <nav>
-                <button id="All" onClick={All} className="All" >All</button>
-                <button id="Apps" onClick={Apps}  className="Apps" >Apps</button>
-                <button id="Webs" onClick={Webs} className="Webs" >Webs</button>
-                <button id="UX_UI" onClick={Expe} className="UX-UI" >UX/UI</button>
+                <button id="All" onClick={All} className="All" ><span id="Sm4-background-All"></span><span>All</span></button>
+                <button id="Apps" onClick={Apps}  className="Apps" ><span id="Sm4-background-Apps"></span><span>Apps</span></button>
+                <button id="Webs" onClick={Webs} className="Webs" ><span id="Sm4-background-Webs"></span><span>Webs</span></button>
+                <button id="UX_UI" onClick={Expe} className="UX-UI" ><span id="Sm4-background-UX_UI"></span><span>UX/UI</span></button>
             </nav>
         </div>
     )
