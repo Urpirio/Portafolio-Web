34417f5d8baa72e225c9f30aa0fc0ec9a576e29d
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-01-01 23:19:43 -0400

    Se agregaron los componentes de la animacion del navegador de la section.

--- src/components/main/Section-main4.jsx
+++ src/components/main/Section-main4.jsx
@@ -4,54 +4,158 @@ import "./style/Section_main4.css";
 const Section_main4 = () =>{
 
     function All(){
+        
+
+        if (document.getElementById("Sm4-background-Apps").style.display == "flex"){
+            document.getElementById("Sm4-background-All").style.display = "flex";
+            document.getElementById("Sm4-background-All").style.animation = "navTranstion1 0.5s"
+            document.getElementById("Sm4-background-Apps").style.display = "none";
+
+        }else{
+            if(document.getElementById("Sm4-background-Webs").style.display == "flex"){
+                document.getElementById("Sm4-background-All").style.display = "flex";
+                document.getElementById("Sm4-background-All").style.animation = "navTranstion2 0.5s";
+                document.getElementById("Sm4-background-Webs").style.display = "none";
+            }else{
+                if(document.getElementById("Sm4-background-UX_UI").style.display == "flex"){
+                    document.getElementById("Sm4-background-All").style.display = "flex";
+                    document.getElementById("Sm4-background-All").style.animation = "navTranstion3 0.5s";
+                    document.getElementById("Sm4-background-UX_UI").style.display = "none";
+                }else{
+                    let NoMoreOption = 1;
+                    if( NoMoreOption == 1){
+                        document.getElementById("Sm4-background-All").style.display = "flex";
+                    };
+                };
+            };
+
+        };
+        if (document.getElementById("Sm4-background-All").style.display == "flex"){
+
         document.getElementById("Web").style.display = "flex";
         document.getElementById("Web2").style.display = "flex";
         document.getElementById("Web3").style.display = "flex";
         document.getElementById("Web4").style.display = "flex";
         document.getElementById("NoContent").style.display = "none";
+        }
 
-        let Apps = document.getElementById("Sm4-background-All").style.background = "linear-gradient(90deg,blue,#0077b6,#90e0ef)";
-        let All = document.getElementById("Sm4-background-Apps").style.background = "none";
-        let Webs = document.getElementById("Sm4-background-Webs").style.background = "none";
-        let Expe = document.getElementById("Sm4-background-UX_UI").style.background = "none";
-    }
+    };
     function Apps(){
 
+        
+
+        if(document.getElementById("Sm4-background-All").style.display == "flex"){
+            document.getElementById("Sm4-background-Apps").style.display = "flex";
+            document.getElementById("Sm4-background-Apps").style.animation = "navTranstion1_Negative 0.5s";
+            document.getElementById("Sm4-background-All").style.display = "none";
+        }else{
+            if(document.getElementById("Sm4-background-Webs").style.display == "flex"){
+                document.getElementById("Sm4-background-Apps").style.display = "flex";
+                document.getElementById("Sm4-background-Apps").style.animation = "navTranstion1 0.5s";
+                document.getElementById("Sm4-background-Webs").style.display = "none";
+            }else{
+                if(document.getElementById("Sm4-background-UX_UI").style.display == "flex"){
+                    document.getElementById("Sm4-background-Apps").style.display = "flex";
+                    document.getElementById("Sm4-background-Apps").style.animation = "navTranstion2 0.5s";
+                    document.getElementById("Sm4-background-UX_UI").style.display = "none";
+                }else{
+                    let NoMoreOption2 = 1;
+                    if(NoMoreOption2 == 1){
+
+                        document.getElementById("Sm4-background-All").style.display = "none";
+                        document.getElementById("Sm4-background-Apps").style.animation = "navTranstion1_Negative 0.5s";
+                        document.getElementById("Sm4-background-Apps").style.display = "flex";
+                    }
+                }
+            }
+        };
+
+        if (document.getElementById("Sm4-background-Apps").style.display == "flex"){
+        
         document.getElementById("Web").style.display = "none";
         document.getElementById("Web2").style.display = "none";
         document.getElementById("Web3").style.display = "none";
         document.getElementById("Web4").style.display = "none";
         document.getElementById("NoContent").style.display = "flex";
 
-        let Apps = document.getElementById("Sm4-background-All").style.background = "none";
-        let All = document.getElementById("Sm4-background-Apps").style.background = "linear-gradient(90deg,blue,#0077b6,#90e0ef)";
-        let Webs = document.getElementById("Sm4-background-Webs").style.background = "none";
-        let Expe = document.getElementById("Sm4-background-UX_UI").style.background = "none";
+        }
     }
     function Webs(){
+
+       if(document.getElementById("Sm4-background-All").style.display == "flex"){
+        document.getElementById("Sm4-background-Webs").style.display = "flex";
+        document.getElementById("Sm4-background-Webs").style.animation = "navTranstion2_Negative 0.5s";
+        document.getElementById("Sm4-background-All").style.display = "none";
+        
+       }else{
+        if(document.getElementById("Sm4-background-Apps").style.display == "flex"){
+            document.getElementById("Sm4-background-Webs").style.display = "flex";
+            document.getElementById("Sm4-background-Webs").style.animation = "navTranstion1_Negative 0.5s";
+            document.getElementById("Sm4-background-Apps").style.display = "none";
+        }else{
+            if(document.getElementById("Sm4-background-UX_UI").style.display == "flex"){
+                document.getElementById("Sm4-background-Webs").style.display = "flex";
+                document.getElementById("Sm4-background-Webs").style.animation = "navTranstion1 0.5s";
+                document.getElementById("Sm4-background-UX_UI").style.display = "none";
+            }else{
+                let NoMoreOption3 = 1;
+                    if(NoMoreOption3 == 1){
+
+                        document.getElementById("Sm4-background-All").style.display = "none";
+                        document.getElementById("Sm4-background-Webs").style.animation = "navTranstion2_Negative 0.5s";
+                        document.getElementById("Sm4-background-Webs").style.display = "flex";
+                        document.getElementById("Sm4-background-All").style.display = "none";
+
+                    }
+            }
+        }
+       };
+       if(document.getElementById("Sm4-background-Webs").style.display == "flex"){
         document.getElementById("Web").style.display = "flex";
         document.getElementById("Web2").style.display = "flex";
         document.getElementById("Web3").style.display = "flex";
         document.getElementById("Web4").style.display = "flex";
         document.getElementById("NoContent").style.display = "none";
-
-        let Apps = document.getElementById("Sm4-background-All").style.background = "none";
-        let All = document.getElementById("Sm4-background-Apps").style.background = "none";
-        let Webs = document.getElementById("Sm4-background-Webs").style.background = "linear-gradient(90deg,blue,#0077b6,#90e0ef)";
-        let Expe = document.getElementById("Sm4-background-UX_UI").style.background = "none";
+       }
     }
-    function Expe(){
-        document.getElementById("Web").style.display = "none";
-        document.getElementById("Web2").style.display = "none";
-        document.getElementById("Web3").style.display = "none";
-        document.getElementById("Web4").style.display = "none";
-        document.getElementById("NoContent").style.display = "flex";
+    function UX_UI(){
+       
+        if(document.getElementById("Sm4-background-All").style.display == "flex"){
+            document.getElementById("Sm4-background-UX_UI").style.display = "flex";
+            document.getElementById("Sm4-background-UX_UI").style.animation = "navTranstion3_Negative 0.5s";
+            document.getElementById("Sm4-background-All").style.display = "none";
+        }else{
+            if(document.getElementById("Sm4-background-Apps").style.display == "flex"){
+                document.getElementById("Sm4-background-UX_UI").style.display = "flex";
+                document.getElementById("Sm4-background-Apps").style.display = "none";
+                document.getElementById("Sm4-background-UX_UI").style.animation = "navTranstion2_Negative 0.5s";
+            }else{
+                if(document.getElementById("Sm4-background-Webs").style.display == "flex"){
+                    document.getElementById("Sm4-background-UX_UI").style.display = "flex";
+                    document.getElementById("Sm4-background-UX_UI").style.animation = "navTranstion1_Negative 0.5s";
+                    document.getElementById("Sm4-background-Webs").style.display = "none";
+                }else{
+                    let NoMoreOption4 = 1;
+                    if(NoMoreOption4 == 1){
 
-        let Apps = document.getElementById("Sm4-background-All").style.background = "none";
-        let All = document.getElementById("Sm4-background-Apps").style.background = "none";
-        let Webs = document.getElementById("Sm4-background-Webs").style.background = "none";
-        let Expe = document.getElementById("Sm4-background-UX_UI").style.background = "linear-gradient(90deg,blue,#0077b6,#90e0ef)";
-    }
+                        document.getElementById("Sm4-background-All").style.display = "none";
+                        document.getElementById("Sm4-background-UX_UI").style.animation = "navTranstion3_Negative 0.5s";
+                        document.getElementById("Sm4-background-UX_UI").style.display = "flex";
+                        document.getElementById("Sm4-background-All").style.display = "none";
+
+                    }
+                }
+            }
+        };
+        
+        if(document.getElementById("Sm4-background-UX_UI").style.display == "flex"){
+            document.getElementById("Web").style.display = "none";
+            document.getElementById("Web2").style.display = "none";
+            document.getElementById("Web3").style.display = "none";
+            document.getElementById("Web4").style.display = "none";
+            document.getElementById("NoContent").style.display = "flex";
+        }
+    };
 
 
 
@@ -62,7 +166,7 @@ const Section_main4 = () =>{
                 <button id="All" onClick={All} className="All" ><span id="Sm4-background-All"></span><span>All</span></button>
                 <button id="Apps" onClick={Apps}  className="Apps" ><span id="Sm4-background-Apps"></span><span>Apps</span></button>
                 <button id="Webs" onClick={Webs} className="Webs" ><span id="Sm4-background-Webs"></span><span>Webs</span></button>
-                <button id="UX_UI" onClick={Expe} className="UX-UI" ><span id="Sm4-background-UX_UI"></span><span>UX/UI</span></button>
+                <button id="UX_UI" onClick={UX_UI} className="UX-UI" ><span id="Sm4-background-UX_UI"></span><span>UX/UI</span></button>
             </nav>
         </div>
     )
