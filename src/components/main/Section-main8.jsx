36616b4f8f63cf09de4db29040ca260cfa858b97
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-01-04 11:36:52 -0400

    Se actulizo el formulario colocandole nuevas regalas al enviar la infomacion a parte de conectando el formulario a la Api.

--- src/components/main/Section-main8.jsx
+++ src/components/main/Section-main8.jsx
@@ -1,3 +1,4 @@
+import { flushSync } from "react-dom";
 import Section_main8P2 from "./Section-main8P2";
 import React, {useState} from "react";
 
@@ -16,61 +17,30 @@ const  Section_main8 = () => {
     }
 
 
-/*Funcion para el envio del furmulario a una base de datos.*/ ;
-var Contador;
-const Sendmessage= () => {
+    const BtnSelectD = ()=>{
+        change_BtnSelect = false;
+        clearInterval(change_BtnSelect1);
+        change_BtnSelect1 = null;
+        console.log("esto corre")
+    };
+    const BtnSelectO = () =>{
+        change_BtnSelect1 = true;
+        clearInterval(change_BtnSelect);
+        change_BtnSelect = null;
+    }
 
- /*Input de la first Name */
- if (document.getElementById("inputtext1").value == ""){
-
-    document.getElementById("Error1").style.display ="flex";
- }else{
-    document.getElementById("Error1").style.display ="none";
-    Contador = 1; 
-    console.log(`Valor1 de contador = ${Contador} `);
- };
- 
- /*Input de Last Name */
- if (document.getElementById("inputtext2").value == ""){
-
-    document.getElementById("Error2").style.display ="flex";
-  }else{
-    document.getElementById("Error2").style.display ="none";
-    Contador = Contador + 1;
-    console.log(`Valor2 de contador = ${Contador} `);
-  };
-  
-  /*Input de email address */
-  if (document.getElementById("inputtext3").value == ""){
-
-    document.getElementById("Error3").style.display ="flex";
-  }else{
-    document.getElementById("Error3").style.display ="none";
-    Contador = Contador + 1;
-    console.log(`Valor3 de contador = ${Contador} `);
-  }
-
-/*Input de Phone Number */
-if (document.getElementById("inputtext4").value == ""){
-    
-    document.getElementById("Error4").style.display ="flex";
-  }else{
-    document.getElementById("Error4").style.display ="none";
-    Contador = Contador + 1;
-    console.log(`Valor4 de contador = ${Contador} `);
-  }
-
- 
-}
-
-
+var change_BtnSelect1;
+var change_BtnSelect;
     const btndefault = () => {
         if(Btn_sltn != "--Please choose a option--" ){
             Btn_sltn.replaceChildren("--Please choose a option--");
             ContainerSelect.style.display = "none";
+            change_BtnSelect = setTimeout(BtnSelectD,1000)
+
         }else{
             Btn_sltn.replaceChildren("--Please choose a option--");
             ContainerSelect.style.display = "none";
+            change_BtnSelect = setTimeout(BtnSelectD,1000)
         };
     };
     
@@ -79,9 +49,11 @@ if (document.getElementById("inputtext4").value == ""){
         if (document.getElementById("btn-selection1") == "--Please choose a option--"){
             Btn_sltn.replaceChildren("Web Design");
             ContainerSelect.style.display = "none";
+            change_BtnSelect1 = setInterval(BtnSelectO,1000);
         }else{
             Btn_sltn.replaceChildren("Web Design");
             ContainerSelect.style.display = "none";
+            change_BtnSelect1 = setInterval(BtnSelectO,1000);
         };
     };
     
@@ -90,9 +62,11 @@ if (document.getElementById("inputtext4").value == ""){
         if (Btn_sltn == "--Please choose a option--"){
             Btn_sltn.replaceChildren("UI/UX Design");
             ContainerSelect.style.display = "none";
+            change_BtnSelect1 = setInterval(BtnSelectO,1000);
         }else{
             Btn_sltn.replaceChildren("UI/UX Design");
             ContainerSelect.style.display = "none";
+            change_BtnSelect1 = setInterval(BtnSelectO,1000);
         };
 
     };
@@ -101,9 +75,11 @@ if (document.getElementById("inputtext4").value == ""){
         if (Btn_sltn == "--Please choose a option--"){
             Btn_sltn.replaceChildren("App Design");
             ContainerSelect.style.display = "none";
+            change_BtnSelect1 = setInterval(BtnSelectO,1000);
         }else{
             Btn_sltn.replaceChildren("App Design");
             ContainerSelect.style.display = "none";
+            change_BtnSelect1 = setInterval(BtnSelectO,1000);
         };
     };
 
@@ -111,13 +87,110 @@ if (document.getElementById("inputtext4").value == ""){
 
         e.preventDefault();
         
-        if (Contador == 4){
-            console.log("Se envio la informacion");
+        if (SendM == 5){
+            const serviceID = 'default_service';
+            const templateID = 'template_9m5c6k3';
+            emailjs.sendform(serviceID,templateID,this).then(()=>{
+                document.getElementById("GlobalError").style.display = "none";
+                document.getElementById("GlobalSend").style.display = "flex";
+            }
+        )
         }else{
             console.log("No se envio la informacion");
         }
     };
 
+    /*Funcion para el envio del furmulario a una base de datos.*/ ;
+var SendM;
+var SendError;
+const Sendmessage= () => {
+
+ /*Input de la first Name */
+ if (document.getElementById("inputtext1").value == ""){
+
+    document.getElementById("Error1").style.display ="flex";
+    SendError = 1;
+
+ }else{
+
+    document.getElementById("Error1").style.display ="none";
+    SendM = 1; 
+    console.log(`Valor1 de contador = ${SendM} `);
+
+ };
+ 
+ /*Input de Last Name */
+ if (document.getElementById("inputtext2").value == ""){
+
+    document.getElementById("Error2").style.display ="flex";
+    SendError = SendError + 1;
+
+  }else{
+
+    document.getElementById("Error2").style.display ="none";
+    SendM = SendM + 1;
+    console.log(`Valor2 de contador = ${SendM} `);
+
+  };
+  
+  /*Input de email address */
+  if (document.getElementById("inputtext3").value == ""){
+
+    document.getElementById("Error3").style.display ="flex";
+    SendError = SendError + 1;
+
+  }else{
+
+    document.getElementById("Error3").style.display ="none";
+    SendM = SendM + 1;
+    console.log(`Valor3 de contador = ${SendM} `);
+
+  }
+
+/*Input de Phone Number */
+if (document.getElementById("inputtext4").value == ""){
+    
+    document.getElementById("Error4").style.display ="flex";
+    SendError = SendError + 1;
+
+  }else{
+
+    document.getElementById("Error4").style.display ="none";
+    SendM = SendM + 1;
+    console.log(`Valor4 de contador = ${SendM} `);
+
+  }
+
+  if(change_BtnSelect1 == true){
+
+    document.getElementById("Error5").style.display ="none";
+    SendM = SendM + 1;
+    console.log(`Valor4 de contador = ${SendM} `);
+
+  }else if(change_BtnSelect == false){
+
+    document.getElementById("Error5").style.display ="flex";
+    SendError = SendError + 1;
+    console.log(SendError);
+
+  }else{
+
+    document.getElementById("Error5").style.display ="flex";
+    SendError = SendError + 1;
+    console.log(SendError);
+
+  }
+
+  if (SendError == 5){
+    document.getElementById("GlobalError").style.display = "flex";
+    document.getElementById("GlobalSend").style.display = "none";
+  }else if(SendM != 5){
+    document.getElementById("GlobalSend").style.display = "none";
+  };
+
+
+
+};
     return(
         <section className="Section-main8" id="Section-main8">
             <form onSubmit={HandleSubmit}  className="form" >
@@ -130,12 +203,12 @@ if (document.getElementById("inputtext4").value == ""){
                 <div className="Form-div2">
 
                     <div>
-                       <input  id="inputtext1" type="text" placeholder="First name" />
+                       <input  id="inputtext1" type="text" name="First_name" placeholder="First name" />
                         <p id="Error1" >Please fill out this field.</p>
                     </div>
 
                     <div>
-                       <input   id="inputtext2" type="text" placeholder="Last name" />
+                       <input   id="inputtext2" type="text" name="Last_name" placeholder="Last name" />
                         <p id="Error2">Please fill out this field.</p>
                     </div>
 
@@ -144,12 +217,12 @@ if (document.getElementById("inputtext4").value == ""){
                 <div className="form-div3" >
 
                     <div>
-                       <input   id="inputtext3" type="email" placeholder="Email address"/>
+                       <input   id="inputtext3" type="email" name="Email_address" placeholder="Email address"/>
                        <p id="Error3" >Please fill out this field.</p>
                     </div>
 
                     <div>
-                       <input   id="inputtext4" type="number" placeholder="Phone number" />
+                       <input   id="inputtext4" type="number" name="Phone number" placeholder="Phone number" />
                        <p id="Error4" >Please fill out this field.</p>
                     </div>
 
@@ -174,15 +247,16 @@ if (document.getElementById("inputtext4").value == ""){
 
                 <div className="form-div5">
 
-                    <textarea placeholder="Message"></textarea>
+                    <textarea placeholder="Message" name="message"></textarea>
+                    <p></p>
 
                 </div>
 
                 <div className="form-btn-sendMessage">
 
                     <button onClick={Sendmessage} type="Submit" >Send Message</button>
-                    <div ><p>One or more fields have an error. Please check and try again.</p></div>
-
+                    <div id="GlobalError" ><p >One or more fields have an error. Please check and try again.</p></div>
+                    <div id="GlobalSend" ><p >Thank you for your message. It has been sent.</p></div>
                 </div>
             </form>
             <Section_main8P2/>
