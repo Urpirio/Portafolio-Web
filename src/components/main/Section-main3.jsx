4751bc6b9e5ad077223cc579f40ba732af4bfd61
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-02-27 12:14:04 -0400

    Se tradujo a español el contenido.

--- src/components/main/Section-main3.jsx
+++ src/components/main/Section-main3.jsx
@@ -3,8 +3,8 @@
 const Section_main3 = () =>{
     return(
          <div className="Section-main3-div1">
-            <h1>My Quality Services</h1>
-            <p>We put your ideas and thus your wishes in the form of a unique web project that inspires you and you customers.</p>
+            <h1>Mis servicios de calidad</h1>
+            <p>Plasmamos sus ideas y, por tanto, sus deseos en forma de un proyecto web único que le inspirará a usted y a sus clientes.</p>
          </div>
     )
 }
